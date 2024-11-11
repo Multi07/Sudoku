66619d4ef0617ac5126ca55f5771df08d96c76c7
kasakj <kubakasak@seznam.cz>
2024-11-11 14:55:23 +0100

    Creation++

--- src/main/java/com/example/sudoku/HelloApplication.java
+++ src/main/java/com/example/sudoku/HelloApplication.java
@@ -3,16 +3,41 @@ package com.example.sudoku;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Sudoku Time!");
+
+
+        GridPane root = new GridPane();
+        //Nefunguje
+        int o =0;
+        int l =0;
+        for (int i =0;i<81;i++) {
+            TextField txt = new TextField();
+            if (o<8) {
+                root.add(txt, o, l);
+                o++;
+            }else {
+                l++;
+                o=0;
+                root.add(txt,o,l);
+
+            }
+        }
+
+        Scene scene = new Scene(root, 320, 240);
+
         stage.setScene(scene);
         stage.show();
     }
